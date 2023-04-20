99ae2e1a52c23c3ffa675668c87cfe7117574f03
Bilaal96 <bilaal-ahmad@hotmail.co.uk>
2023-04-20 09:25:01 +0100

    dep: add and configure react-query

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 // Contexts
+import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from './contexts/Auth';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
 // Components
 import CSSBaseline from '@mui/material/CssBaseline';
-import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
 import '@fontsource/roboto/300.css';
@@ -14,14 +15,18 @@ import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 
+const queryClient = new QueryClient();
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <AuthProvider>
-        <CSSBaseline />
-        <App />
-      </AuthProvider>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <AuthProvider>
+          <CSSBaseline />
+          <App />
+        </AuthProvider>
+      </BrowserRouter>
+    </QueryClientProvider>
   </React.StrictMode>
 );
