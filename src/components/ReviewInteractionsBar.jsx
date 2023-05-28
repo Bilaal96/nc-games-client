cc4d42f17be03b6f219ee388606e0db6c6cd0a36
Bilaal96 <bilaal-ahmad@hotmail.co.uk>
2023-05-28 17:26:36 +0100

    feat: persist user votes to & initalise user votes state from localStorage

--- src/components/ReviewInteractionsBar.jsx
+++ src/components/ReviewInteractionsBar.jsx
@@ -1,4 +1,11 @@
-import { useState } from 'react';
+import { useContext } from 'react';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import useLocalStorage from '../hooks/useLocalStorage';
+
+// Contexts
+import { AuthContext } from '../contexts/Auth';
+
+// Components
 import { Box, IconButton, Stack } from '@mui/material';
 import {
   ThumbUp,
@@ -6,18 +13,22 @@ import {
   ThumbDown,
   ThumbDownOutlined,
 } from '@mui/icons-material';
-import { useMutation, useQueryClient } from '@tanstack/react-query';
+
 import * as gamesApi from '../api';
 
 const ReviewInteractionsBar = ({ review }) => {
-  const { review_id, votes } = review;
-  const [isLiked, setIsLiked] = useState(false);
-  const [isDisliked, setIsDisliked] = useState(false);
+  const { user } = useContext(AuthContext);
+  const { review_id } = review;
+
+  const [votes, setVotes] = useLocalStorage(
+    `${user.username}-review-${review_id}`,
+    { isLiked: false, isDisliked: false }
+  );
 
   const queryClient = useQueryClient();
   const votesMutation = useMutation({
     mutationFn: gamesApi.patchReviewVotes,
-    onSuccess: () => {
+    onSuccess: (updatedReview) => {
       // invalidateQuery -> make reviews data stale, which in turn forces a refetch
       queryClient.invalidateQueries(['reviews']);
     },
@@ -25,73 +36,69 @@ const ReviewInteractionsBar = ({ review }) => {
 
   // Upvote/downvote functions determine what argument to invoke the mutationFn with
   const handleUpvoteClick = () => {
-    if (isLiked) {
+    if (votes.isLiked) {
       // Already liked -> click removes 1 vote -> setIsLiked(false)
       votesMutation
         .mutateAsync({ review_id, inc_votes: -1 })
         .then(() => {
-          setIsLiked(false);
+          setVotes((prevVotes) => ({ ...prevVotes, isLiked: false }));
         })
         .catch((err) => {
-          setIsLiked(true);
+          setVotes((prevVotes) => ({ ...prevVotes, isLiked: true }));
         });
-    } else if (isDisliked) {
+    } else if (votes.isDisliked) {
       // Already disliked -> click adds 2 votes -> increment by 2 -> setIsLiked(true) & setIsDisliked(false)
       votesMutation
         .mutateAsync({ review_id, inc_votes: 2 })
         .then(() => {
-          setIsLiked(true);
-          setIsDisliked(false);
+          setVotes({ isLiked: true, isDisliked: false });
         })
         .catch((err) => {
-          setIsLiked(false);
-          setIsDisliked(true);
+          setVotes({ isLiked: false, isDisliked: true });
         });
     } else {
       // Neither liked/disliked -> click adds 1 vote -> increment by 1 -> setIsLiked(true)
       votesMutation
         .mutateAsync({ review_id, inc_votes: 1 })
         .then(() => {
-          setIsLiked(true);
+          setVotes((prevVotes) => ({ ...prevVotes, isLiked: true }));
         })
         .catch((err) => {
-          setIsLiked(false);
+          setVotes((prevVotes) => ({ ...prevVotes, isLiked: false }));
         });
     }
   };
 
   const handleDownvoteClick = () => {
-    if (isDisliked) {
+    if (votes.isDisliked) {
       // Already disliked -> click adds 1 vote
       votesMutation
         .mutateAsync({ review_id, inc_votes: 1 })
         .then(() => {
-          setIsDisliked(false);
+          setVotes((prevVotes) => ({ ...prevVotes, isDisliked: false }));
         })
         .catch((err) => {
-          setIsDisliked(true);
+          setVotes((prevVotes) => ({ ...prevVotes, isDisliked: true }));
         });
-    } else if (isLiked) {
+    } else if (votes.isLiked) {
       // Already liked -> click removes 2 votes
       votesMutation
         .mutateAsync({ review_id, inc_votes: -2 })
         .then(() => {
-          setIsDisliked(true);
-          setIsLiked(false);
+          setVotes({ isLiked: false, isDisliked: true });
         })
         .catch((err) => {
-          setIsDisliked(false);
-          setIsLiked(true);
+          setVotes({ isLiked: true, isDisliked: false });
         });
     } else {
       // Neither liked/disliked -> click removes 1 vote
       votesMutation
         .mutateAsync({ review_id, inc_votes: -1 })
         .then(() => {
-          setIsDisliked(true);
+          setVotes((prevVotes) => ({ ...prevVotes, isDisliked: true }));
         })
         .catch((err) => {
-          setIsDisliked(false);
+          setVotes((prevVotes) => ({ ...prevVotes, isDisliked: false }));
         });
     }
   };
@@ -103,11 +110,9 @@ const ReviewInteractionsBar = ({ review }) => {
         direction="row"
         gap={1}
         sx={{
-          border: '1px solid rgba(0,0,0,.4)',
-          // color: 'white',
+          border: '1px solid rgba(0,0,0,0.4)',
           p: 1,
           borderRadius: '3rem',
-          // mx: 'auto',
           textAlign: 'center',
           width: '20rem',
         }}
@@ -120,16 +125,20 @@ const ReviewInteractionsBar = ({ review }) => {
           sx={iconButtonStyle}
           aria-label="like"
         >
-          {isLiked ? <ThumbUp color="primary" /> : <ThumbUpOutlined />}
+          {votes.isLiked ? <ThumbUp color="primary" /> : <ThumbUpOutlined />}
         </IconButton>
-        <Box sx={{ fontWeight: 'bold' }}>{votes} votes</Box>
+        <Box sx={{ fontWeight: 'bold' }}>{review.votes} votes</Box>
         <IconButton
           onClick={handleDownvoteClick}
           disabled={votesMutation.isLoading}
           sx={iconButtonStyle}
           aria-label="dislike"
         >
-          {isDisliked ? <ThumbDown color="primary" /> : <ThumbDownOutlined />}
+          {votes.isDisliked ? (
+            <ThumbDown color="primary" />
+          ) : (
+            <ThumbDownOutlined />
+          )}
         </IconButton>
       </Stack>
     </Box>
